915e4d33ce205d3d368250747656f876a2e1ba48
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-03-29 21:09:03 +0300

    Commit message

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,64 +1,43 @@
 <template>
   <div id="app" fixed>
     <div id=" nav" class="bg-gradient-to-r from-gray-100 to-gray-300 py-2">
-      <nav
-        class="container px-6 py-6 mx-auto md:flex md:justify-between md:items-center"
-      >
+      <nav class="container px-6 py-6 mx-auto md:flex md:justify-between md:items-center">
         <div class="flex items-center justify-between">
-          <nuxt-link
-            to="/"
-            class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-500 md:text-2xl hover:text-blue-200"
-            >DEAMS
+          <nuxt-link to="/"
+            class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-500 md:text-2xl hover:text-blue-200">DEAMS
           </nuxt-link>
           <!-- Mobile menu button -->
           <div @click="toggleNav" class="flex-end md:hidden">
-            <button
-              type="button"
-              class="text-white hover:text-gray-1000 focus:outline-none focus:text-gray-400"
-            >
+            <button type="button" class="text-white hover:text-gray-1000 focus:outline-none focus:text-gray-400">
               <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-                <path
-                  fill-rule="evenodd"
-                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                ></path>
+                <path fill-rule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
+                </path>
               </svg>
             </button>
           </div>
         </div>
 
         <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-        <ul
-          :class="showMenu ? 'flex' : 'hidden'"
-          class="flex-col mt-4 m-2 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
-        >
+        <ul :class="showMenu ? 'flex' : 'hidden'"
+          class="flex-col mt-4 m-2 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
           <li>
             <nuxt-link class="text-black text-sm hover:text-blue-300" to="/">
-              Home</nuxt-link
-            >
+              Home</nuxt-link>
           </li>
           <li>
-            <nuxt-link
-              class="text-black text-sm hover:text-blue-300"
-              to="OurService"
-              >Our Services
+            <nuxt-link class="text-black text-sm hover:text-blue-300" to="OurService">Our Services
             </nuxt-link>
           </li>
           <li>
-            <nuxt-link
-              class="text-black text-sm hover:text-blue-300"
-              to="About"
-            >
+            <nuxt-link class="text-black text-sm hover:text-blue-300" to="About">
               About us
             </nuxt-link>
           </li>
           <li class="pt-2">
-            <button
-              type="button"
-              class="text-white shadow-lg bg-blue-300 hover:-translate-y-1 hover:scale-110 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-4 text-center mr-2 mb-2"
-            >
-              <Nuxt-link class="flex text-white" to="Contact"
-                >Work with Us</Nuxt-link
-              >
+            <button type="button"
+              class="text-white shadow-lg bg-blue-300 hover:-translate-y-1 hover:scale-110 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-4 text-center mr-2 mb-2">
+              <Nuxt-link class="flex text-white" to="Contact">Work with Us</Nuxt-link>
             </button>
           </li>
         </ul>
@@ -66,7 +45,7 @@
     </div>
   </div>
 </template>
-  
+
 <script>
 import { ref } from "vue";
 
@@ -84,7 +63,7 @@ export default {
   },
 };
 </script>
-  
+
 <style lang="css">
 .scale-slide-enter-active,
 .scale-slide-leave-active {
@@ -108,4 +87,3 @@ export default {
   transform: scale(0.8);
 }
 </style>
-  
