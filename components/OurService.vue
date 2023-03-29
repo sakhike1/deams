8248c506bc0f451882d03e2f54c4d788e97b9301
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-03-29 11:00:50 +0300

    Commit message

--- components/OurService.vue
+++ components/OurService.vue
@@ -35,7 +35,7 @@
                                 speak directly to our UX/UI team.
                             </p>
 
-                            <a href="#_"
+                            <a href="#"
                                 class="relative mt-5 inline-flex items-center justify-start px-6 py-3 overflow-hidden border font-medium transition-all bg-white rounded hover:bg-white group">
                                 <span
                                     class="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
@@ -47,7 +47,7 @@
                         </div>
 
                         <div>
-                            <img data-aos="fade-up" data-aos-duration="3000" class="w-full" src="~/assets/Frame 1.png"
+                            <img data-aos="fade-up" data-aos-duration="3000" class="w-full" src="~/assets/coding.png"
                                 alt="" />
                         </div>
                     </div>
