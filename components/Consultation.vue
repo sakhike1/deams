8336ab25c8af684c5ebe8c3c0967d063abf86f4a
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-04-04 23:17:55 +0300

    update files

--- components/Consultation.vue
+++ components/Consultation.vue
@@ -1,35 +1,26 @@
 <template>
-    <section data-aos="fade-right" data-aos-duration="3000" class=" 2xl:py-24 2xl:bg-gray-50">
+    <section data-aos="fade-right" data-aos-duration="3000" class="2xl:py-24 2xl:bg-gray-50">
         <div
             class="px-4 shadow-xl mx-auto Pastel bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 max-w-7xl sm:px-6 lg:px-8 2xl:rounded-xl">
             <div class="py-10 sm:py-16 lg:py-24 2xl:pl-24">
                 <div class="grid items-center grid-cols-1 gap-y-8 lg:grid-cols-2 lg:gap-x-8 2xl:gap-x-20">
                     <div class="lg:order-2 2xl:-mr-24">
-                        <img class="w-full h-80 shadow-xl  rounded-xl"
+                        <img class="w-full h-80 shadow-xl rounded-xl"
                             src="~/assets/2209.i201.035.S.m004.c13.mobile app development isometric.jpg" alt="" />
                     </div>
 
                     <div class="lg:order-1">
                         <h2 class="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl lg:leading-snug">
-                            Start working on <br class="hidden xl:block" />your App right now </h2>
-
-
-
-
-
-
-
-
+                            Start working on <br class="hidden xl:block" />your App right now
+                        </h2>
 
                         <div class="flex flex-col items-start mt-8 sm:space-x-4 sm:flex-row sm:items-center lg:mt-12">
-
-
                             <a href="#_"
                                 class="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
                                 <span
                                     class="w-full h-full bg-gradient-to-br from-[#454545] via-[#454545] to-[#454545] group-hover:from-[#00FF00] group-hover:via-[#00FF00] group-hover:to-[#00FF00] absolute"></span>
                                 <span
-                                    class="relative px-6 py-3 transition-all ease-out  rounded-md group-hover:bg-opacity-0 duration-400">
+                                    class="relative px-6 py-3 transition-all ease-out rounded-md group-hover:bg-opacity-0 duration-400">
                                     <span class="relative text-white">Learn More</span>
                                 </span>
                             </a>
@@ -48,7 +39,5 @@ export default {
     mounted() {
         AOS.init({});
     },
-}
-
-
+};
 </script>
--- components/Contact.vue
+++ components/Contact.vue
@@ -29,7 +29,7 @@
                                 <form>
                                     <div class="form-group mb-6">
                                         <input required="required" v-model="form.surname" type="text"
-                                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border    rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                             id="exampleInput7" placeholder="Name" />
                                         <div />
                                     </div>
@@ -170,6 +170,10 @@ export default {
 <style>
 .max-w-\[85rem\] {
     max-width: 125rem;
+}
+
+.form-control {
+    border: border solid red 1px;
 }
 </style>
   
