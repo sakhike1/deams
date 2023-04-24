0c5488ec6ec2186f15f4a4ccd8ce86006c0225e7
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-04-24 18:21:47 +0300

    updated button

--- components/Deams.vue
+++ components/Deams.vue
@@ -39,8 +39,8 @@
 
 
                   <nuxt-link to="Contact" title=""
-                    class="inline-flex hover:scale-75 items-center justify-center px-4 py-4 mt-5 text-base font-semibold text-white transition-all duration-200 bg-transparent border border-white rounded-md sm:mt-0 hover:bg-white hover:text-black"
-                    role="button"> Reach out to deams</nuxt-link>
+                    class="inline-flex hover:scale-75 items-center justify-center px-4 py-4 mt-5 text-base font-semibold text-white transition-all duration-200 bg-transparent rounded-full border-white border sm:mt-0 hover:bg-white hover:text-black"
+                    role="button"> Reach out to us</nuxt-link>
                 </div>
               </div>
             </div>
