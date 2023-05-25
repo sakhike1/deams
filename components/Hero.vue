acbdc4d177de4c3973bbf7ea4ca27c5eebca83ca
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-05-26 01:25:03 +0300

    update hero page

--- components/Hero.vue
+++ components/Hero.vue
@@ -10,20 +10,21 @@
                 <div class="md:7/12 lg:w-1/2">
                     <h2 class="text-2xl font-bold text-white md:text-2xl dark:text-white">
                         Our work is carried
-                        <span class=""> out by
-                            passionate <br> developers </span>
+                        out by
+                        passionate <br> developers
                     </h2>
 
                     <div class="divide-y space-y-4 divide-gray-100 dark:divide-gray-800">
                         <div class="mt-8 flex gap-4 md:items-center">
-                            <div class="w-12 h-12 flex gap-4 rounded-full bg-indigo-100 dark:bg-indigo-900/20">
+                            <nuxt-link to="contact"
+                                class="w-12 h-12 flex hover:scale-95 gap-4 rounded-full bg-indigo-100 dark:bg-indigo-900/20">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                    class="w-6 h-6 m-auto text-indigo-500 dark:text-indigo-400">
+                                    class="w-6 h-6 m-auto text-indigo-500  dark:text-indigo-400">
                                     <path fill-rule="evenodd"
                                         d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97zM6.75 8.25a.75.75 0 01.75-.75h9a.75.75 0 010 1.5h-9a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H7.5z"
                                         clip-rule="evenodd" />
                                 </svg>
-                            </div>
+                            </nuxt-link>
                             <div class="w-5/6">
                                 <h4 class="font-semibold text-lg  text-white">
                                     Chat to us
